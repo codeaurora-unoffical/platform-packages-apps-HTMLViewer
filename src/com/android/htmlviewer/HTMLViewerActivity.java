af65969e19a2b9781757f4340320450671bb6719
Libo Jin <liboj@codeaurora.org>
2016-06-28 13:14:10 +0800

    Update permission usage for M
    Change-Id: I6a08e807d5c7fb12196fa86d771041e72d7b2def
    CRs-Fixed: 1031612

--- src/com/android/htmlviewer/HTMLViewerActivity.java
+++ src/com/android/htmlviewer/HTMLViewerActivity.java
@@ -17,8 +17,11 @@
 package com.android.htmlviewer;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,9 +32,11 @@ import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -44,6 +49,7 @@ public class HTMLViewerActivity extends Activity {
 
     private WebView mWebView;
     private View mLoading;
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,12 +77,46 @@ public class HTMLViewerActivity extends Activity {
         s.setJavaScriptEnabled(false);
         s.setDefaultTextEncodingName("utf-8");
 
-        final Intent intent = getIntent();
-        if (intent.hasExtra(Intent.EXTRA_TITLE)) {
-            setTitle(intent.getStringExtra(Intent.EXTRA_TITLE));
-        }
+        mIntent = getIntent();
+        requestPermissionAndLoad();
+    }
 
-        mWebView.loadUrl(String.valueOf(intent.getData()));
+    private void loadUrl() {
+        if (mIntent.hasExtra(Intent.EXTRA_TITLE)) {
+            setTitle(mIntent.getStringExtra(Intent.EXTRA_TITLE));
+        }
+        mWebView.loadUrl(String.valueOf(mIntent.getData()));
+    }
+
+    private void requestPermissionAndLoad() {
+        Uri destination = mIntent.getData();
+        if (destination != null) {
+            // Is this a local file?
+            if ("file".equals(destination.getScheme())
+                        && PackageManager.PERMISSION_DENIED ==
+                                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            } else {
+                loadUrl();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+            String permissions[], int[] grantResults) {
+        // We only ever request 1 permission, so these arguments should always have the same form.
+        assert permissions.length == 1;
+        assert Manifest.permission.READ_EXTERNAL_STORAGE.equals(permissions[0]);
+
+        if (grantResults.length == 1 && PackageManager.PERMISSION_GRANTED == grantResults[0]) {
+            // Try again now that we have the permission.
+            loadUrl();
+        } else {
+            Toast.makeText(HTMLViewerActivity.this,
+                    R.string.turn_on_storage_permission, Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     @Override
